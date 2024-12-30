7950ead71c24477d3ef8b8f28fd9bed345760c12
Michael Chrupcala <michael@artblocks.io>
2024-12-30 14:38:16 -0500

    displaying image from S3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "strange-luck.s3.us-east-1.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
