5e3e3b385a69919a11aa4109cda8a0746d64c8f5
Michael Chrupcala <michael@artblocks.io>
2025-04-24 15:47:38 -0400

    rmvd Favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,6 +108,7 @@ export default function Home() {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           rel="preload"
           as="video"
