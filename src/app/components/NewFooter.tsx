f92addce3b2bc965a1c2c92bf9d6c0f3bbe26248
Michael Chrupcala <michael@artblocks.io>
2025-05-13 18:03:44 -0400

    fixed build errors

--- src/app/components/NewFooter.tsx
+++ src/app/components/NewFooter.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function NewFooter() {
   return (
     <footer className="hidden sm:flex justify-between w-full text-white text-glow-extra-small px-10 py-14 text-xl">
