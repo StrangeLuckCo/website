3e1badd24aef162b8312c6a3b4d4825578b2dd93
Michael Chrupcala <michael@artblocks.io>
2025-05-13 23:01:55 -0400

    fixed mobile redirect design and enabled email campaign

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -18,7 +18,6 @@ declare global {
 }
 
 export default function Welcome() {
-  const [email, setEmail] = useState("");
   const [isReady, setIsReady] = useState(false);
   const [shouldShowUnicorn, setShouldShowUnicorn] = useState(false);
   const isMobile = useIsMobile();
@@ -38,43 +37,29 @@ export default function Welcome() {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="relative pt-[110px] flex flex-col z-20 gap-y-[55px] sm:gap-y-[120px] w-4/5 items-center">
+      <div className="relative pt-[40px] sm:pt-[90px] flex flex-col z-20 gap-y-[55px] sm:gap-y-[100px] w-4/5 items-center">
         <h2 className="sl-h2-mobile text-center sm:sl-h2 eighties-glow text-5xl">
           A new, strange future is coming soon.
         </h2>
 
-        <div className="flex flex-col w-full justify-center items-center gap-y-16 sm:gap-y-8">
+        <div className="flex flex-col w-full justify-center items-center gap-y-8 sm:gap-y-10">
           <h3 className="sl-h3-mobile sm:sl-h3 eighties-glow text-5xl">
             Don&#39;t miss it.
           </h3>
           <form
-            onSubmit={async (e) => {
-              e.preventDefault();
-
-              const res = await fetch("/api/contact", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(email),
-              });
-
-              const result = await res.json();
-              if (result.success) {
-                console.log("Message sent successfully");
-              } else {
-                alert("Something went wrong. Please try again.");
-              }
-            }}
+            action="https://yourstrangeluck.us3.list-manage.com/subscribe/post"
+            method="POST"
+            target="_blank"
+            noValidate
             className="w-full sm:border-2 border-[#D9D9D9] flex flex-col sm:flex-row gap-y-10 sm:gap-y-0 justify-between max-w-[650px] h-full sm:items-stretch items-center"
           >
+            <input type="hidden" name="u" value="91ffec2a68b8a49943dc85b46" />
+            <input type="hidden" name="id" value="f23e80cd72" />
             <input
               type="email"
-              id="email"
+              name="MERGE0"
+              id="MERGE0"
               placeholder="Enter your email"
-              value={email}
-              onChange={(e) => {
-                console.log(email);
-                setEmail(e.target.value);
-              }}
               className="sl-p desktop-body input-box-glow w-full sm:w-4/5 flex flex-1 bg-black p-2 border-2 border-[#D9D9D9] sm:border-0 text-center sm:text-left cursor-[url('/hand_cursor.png'),_pointer]"
               required
             />
@@ -89,7 +74,11 @@ export default function Welcome() {
       {isReady && shouldShowUnicorn ? (
         <div
           id="unicorn-welcome"
-          className="absolute top-0 left-0 w-full min-h-[700px] h-full z-0 unicorn-embed"
+          className="absolute top-0 left-0 w-full min-h-[700px] h-full max-w-screen overflow-hidden z-0"
+          style={{
+            maxWidth: "100vw",
+            overflow: "hidden",
+          }}
         />
       ) : isReady ? (
         <Image
@@ -100,7 +89,7 @@ export default function Welcome() {
           }
           fill
           alt="Black background with 'Strange Luck: A Storytelling Studio' text"
-          className="absolute inset-0 w-full h-full object-cover"
+          className="object-cover"
         />
       ) : null}
     </div>
