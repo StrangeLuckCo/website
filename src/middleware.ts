b99a0e15c2f5d2d924826492d00821ac220a25e2
Michael Chrupcala <michael.chrupcala@gmail.com>
2025-05-20 22:55:02 -0400

    middleware temp change

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function middleware() {
   //TODO: restore the re-direct before I merge this branch with /main
   // const { pathname } = request.nextUrl;
 
